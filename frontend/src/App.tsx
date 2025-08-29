e0b6668eea63412b96c2b9524d1a54c3ca99e870
Damian Stewart <d@damianstewart.com>
2025-08-29 20:53:52 +0200

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,7 @@ import {Tab, TabList, TabPanel, Tabs} from 'react-tabs';
 import '@/Style/Tabs.css';
 import '@/Style/Collapsible.css'
 
-import PopulateDatabase from "@/Components/PopulateDatabase.tsx";
-import { Collapsible } from "@/Components/ui/Collapsible.tsx";
 import DistanceQuery from "@/Components/DistanceQuery.tsx";
-import ZeroShotClassificationQuery from "@/Components/ZeroShotClassificationQuery.tsx";
 import Image from "@/types/image";
 import {useState} from "react";
 import {TagEditorSidebar} from "@/Components/TagEditorSidebar.tsx";
@@ -42,39 +39,15 @@ function App() {
                     <h1>Image Search</h1>
                     
                     <div className="space-y-6">
-                        <Collapsible trigger="Populate Database">
-                            <PopulateDatabase/>
-                        </Collapsible>
-                        
-                        <Collapsible trigger="Tag Filters">
-                            <p className="text-slate-600">Tag filters go here</p>
-                        </Collapsible>
-
                         <div className="bg-white rounded-lg border border-slate-200 shadow-sm">
-                            <Tabs>
-                              <TabList>
-                                  <Tab>Distance</Tab>
-                                  <Tab>Zero-Shot Classification</Tab>
-                              </TabList>
-
-                              <TabPanel>
-                                  <DistanceQuery 
-                                    setSelectedImages={setSelectedImages}
-                                    onRevealInFinder={handleRevealInFinder}
-                                    thumbnailSize={thumbnailSize}
-                                    gridHasFocus={gridHasFocus}
-                                    onGridFocusChange={setGridHasFocus}
-                                    onResultCountsChange={setResultCounts}
-                                  />
-                              </TabPanel>
-                              {/*<TabPanel>
-                                  <ZeroShotClassificationQuery 
-                                    setSelectedImages={setSelectedImages}
-                                    thumbnailSize={thumbnailSize}
-                                    onGridFocusChange={setGridHasFocus}
-                                  />
-                              </TabPanel>*/}
-                          </Tabs>
+                            <DistanceQuery 
+                            setSelectedImages={setSelectedImages}
+                            onRevealInFinder={handleRevealInFinder}
+                            thumbnailSize={thumbnailSize}
+                            gridHasFocus={gridHasFocus}
+                            onGridFocusChange={setGridHasFocus}
+                            onResultCountsChange={setResultCounts}
+                            />
                         </div>
                     </div>
               </div>
